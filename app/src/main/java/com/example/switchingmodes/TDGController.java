c69f80116a883039b2808eafee2477ba06a9ebaa
Benjamin <benjamin.tran@bellevuecollege.edu>
2020-11-30 16:52:36 -0800

    third commit

---
+++ app/src/main/java/com/example/switchingmodes/TDGController.java
@@ -0,0 +1,103 @@
+package com.example.switchingmodes;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.hardware.SensorManager;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.SystemClock;
+import android.view.View;
+import android.widget.Button;
+//import android.widget.CompoundButton;
+//import android.widget.ToggleButton;
+
+import java.io.File;
+import java.io.IOException;
+
+public class TDGController extends AppCompatActivity {
+    private TDGModel tdgMod;
+    //private ToggleButton tbLogMode;
+    //File f;
+    private Button btnInput1;
+    private Button btnInput2;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        File senf = new File(getExternalFilesDir(null), "tdgsen.txt");
+        File inpf = new File(getExternalFilesDir(null), "tdginp.txt");
+        try {
+            tdgMod = new TDGModel((SensorManager) getSystemService(SENSOR_SERVICE), senf, inpf);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        setContentView(R.layout.tdg_view);
+        btnInput1 = (Button)findViewById(R.id.input1);
+        btnInput2 = (Button)findViewById(R.id.input2);
+
+        btnInput1.setOnClickListener(new View.OnClickListener(){
+
+                                         @Override
+                                         public void onClick(View v) {
+                                             if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR1) {
+                                                 tdgMod.input('1', SystemClock.elapsedRealtimeNanos());
+                                             }
+                                         }
+                                     }
+
+        );
+
+        btnInput2.setOnClickListener(new View.OnClickListener(){
+
+                                         @Override
+                                         public void onClick(View v) {
+                                             if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR1) {
+                                                 tdgMod.input('2', SystemClock.elapsedRealtimeNanos());
+                                             }
+                                         }
+                                     }
+
+        );
+
+        try {
+            tdgMod.setLogging(true);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+/*
+        tbLogMode = (ToggleButton)findViewById(R.id.tbLogMode);
+        tbLogMode.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+             @Override
+             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                 //Intent i =
+
+                 try {
+                     tdgMod.setLogging(isChecked);
+                 } catch (IOException e) {
+                     e.printStackTrace();
+                 }
+
+                 if(i!=null){
+                     startActivity(i);
+                 }
+
+             }
+         }
+
+        );*/
+    }
+
+    @Override
+    protected void onDestroy() {
+        try {
+            tdgMod.setLogging(false);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        tdgMod.close();
+        super.onDestroy();
+    }
+
+
+}
---
+++ app/src/main/java/com/example/switchingmodes/TDGModel.java
@@ -0,0 +1,121 @@
+package com.example.switchingmodes;
+
+
+import android.hardware.Sensor;
+import android.hardware.SensorDirectChannel;
+import android.hardware.SensorManager;
+import android.os.MemoryFile;
+
+import java.io.DataOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class TDGModel {
+    private class PressEvent{
+        private char press;
+        private long timestamp;
+
+        public PressEvent(char p, long t){
+            press = p;
+            timestamp = t;
+        }
+
+        public char getPress(){
+            return press;
+        }
+
+        public long getTimestamp() {
+            return timestamp;
+        }
+    }
+
+    private SensorManager mSensorManager;
+    private Sensor mGyro;
+    private MemoryFile mem;
+    private SensorDirectChannel mSDC;
+    private File sensorFile;
+    private File inputFile;
+    private Queue<PressEvent> peQ;
+
+    public TDGModel(SensorManager sm, File senf, File inpf) throws IOException {
+        mSensorManager = sm;
+        sensorFile = senf;
+        inputFile = inpf;
+        mGyro = mSensorManager.getDefaultSensor(Sensor.TYPE_GYROSCOPE);
+
+        mem = new MemoryFile(null, 1048575);
+
+        mSDC = sm.createDirectChannel(mem);
+        peQ  = new LinkedList<PressEvent>();
+
+    }
+
+    public void input(char pressed, long timestamp){
+        peQ.add(new PressEvent(pressed,timestamp));
+    }
+
+    public void setLogging(boolean b) throws IOException {
+        if(b)
+        {
+            mSDC.configure(mGyro,SensorDirectChannel.RATE_NORMAL);
+            System.out.println("on");
+            //return null;
+        }
+        else
+        {
+            mSDC.configure(mGyro,SensorDirectChannel.RATE_STOP);
+            InputStream is = mem.getInputStream();
+            //Path path;
+            FileOutputStream fos = new FileOutputStream(sensorFile);
+
+            byte[] buf = new byte[8192];
+            int length;
+            while ((length = is.read(buf)) > 0) {
+                fos.write(buf, 0, length);
+            }
+            fos.close();
+            FileOutputStream fos2 = new FileOutputStream(inputFile);
+            DataOutputStream dos2 = new DataOutputStream(fos2);
+            while(!peQ.isEmpty()){
+                PressEvent pe = peQ.remove();
+                dos2.writeChar(pe.getPress());
+                dos2.writeLong(pe.getTimestamp());
+
+            }
+            dos2.close();
+            fos2.close();
+
+
+
+
+            //Files.copy(is,path);
+
+            //File exportFile = new File("/storage/emulated/0/Download/keyspy/loggerdemo.logger");
+            //Intent i = new Intent(Intent.ACTION_CREATE_DOCUMENT);
+
+            /*
+            try {
+                //Path path = FileSystems.getDefault().getPath("keyspy", "logger", "foo.bar");
+
+                //Files.createDirectories(path.getParent());
+                //Files.createFile(path);
+                //new File(path.toString());
+
+                //Files.copy(is, path);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+             */
+            System.out.println("off");
+            //return i;
+        }
+    }
+
+    public void close(){
+        mSDC.close();
+    }
+}
