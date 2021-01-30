7cb909515052aee2b5935d83bb267759f4017a41
sean.sands <sean.sands@bellevuecollege.edu>
2021-01-29 17:41:54 -0800

    forkyfork

--- app/src/main/java/com/example/switchingmodes/TDGController.java
+++ app/src/main/java/com/example/switchingmodes/TDGController.java
@@ -9,17 +9,54 @@ import android.os.Bundle;
 import android.os.SystemClock;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 //import android.widget.CompoundButton;
 //import android.widget.ToggleButton;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 public class TDGController extends AppCompatActivity {
     private TDGModel tdgMod;
 
-    private Button btnInput1;
-    private Button btnInput2;
+    //private Button btnInput1;
+    //private Button btnInput2;
+
+    private HashMap<Character, Button> buttons;
+
+    private void keyboardSetup() {
+        // first row of keys
+        buttons.put('q', (Button)findViewById(R.id.qButton));
+        buttons.put('w', (Button)findViewById(R.id.wButton));
+        buttons.put('e', (Button)findViewById(R.id.eButton));
+        buttons.put('r', (Button)findViewById(R.id.rButton));
+        buttons.put('t', (Button)findViewById(R.id.tButton));
+        buttons.put('y', (Button)findViewById(R.id.yButton));
+        buttons.put('u', (Button)findViewById(R.id.uButton));
+        buttons.put('i', (Button)findViewById(R.id.iButton));
+        buttons.put('o', (Button)findViewById(R.id.oButton));
+        buttons.put('p', (Button)findViewById(R.id.pButton));
+        // second row of keys
+        buttons.put('a', (Button)findViewById(R.id.aButton));
+        buttons.put('s', (Button)findViewById(R.id.sButton));
+        buttons.put('d', (Button)findViewById(R.id.dButton));
+        buttons.put('f', (Button)findViewById(R.id.fButton));
+        buttons.put('g', (Button)findViewById(R.id.gButton));
+        buttons.put('h', (Button)findViewById(R.id.hButton));
+        buttons.put('j', (Button)findViewById(R.id.jButton));
+        buttons.put('k', (Button)findViewById(R.id.kButton));
+        buttons.put('l', (Button)findViewById(R.id.lButton));
+        // third row of keys
+        buttons.put('z', (Button)findViewById(R.id.zButton));
+        buttons.put('x', (Button)findViewById(R.id.xButton));
+        buttons.put('c', (Button)findViewById(R.id.cButton));
+        buttons.put('v', (Button)findViewById(R.id.vButton));
+        buttons.put('b', (Button)findViewById(R.id.bButton));
+        buttons.put('n', (Button)findViewById(R.id.nButton));
+        buttons.put('m', (Button)findViewById(R.id.mButton));
+    }
 
     @RequiresApi(api = Build.VERSION_CODES.O)
     @Override
@@ -36,9 +73,23 @@ public class TDGController extends AppCompatActivity {
             e.printStackTrace();
         }
         setContentView(R.layout.tdg_view);
-        btnInput1 = (Button)findViewById(R.id.input1);
-        btnInput2 = (Button)findViewById(R.id.input2);
 
+        buttons = new HashMap<>();
+
+        keyboardSetup();
+
+        for (Map.Entry<Character, Button> entry : buttons.entrySet()) {
+            entry.getValue().setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    tdgMod.input(entry.getKey(), SystemClock.elapsedRealtimeNanos());
+                }
+            });
+        }
+
+        //btnInput1 = (Button)findViewById(R.id.input1);
+        //btnInput2 = (Button)findViewById(R.id.input2);
+        /*
         btnInput1.setOnClickListener(new View.OnClickListener(){
 
                                          @Override
@@ -62,33 +113,18 @@ public class TDGController extends AppCompatActivity {
                                      }
 
         );
-
+        */
         try {
             tdgMod.setLogging(true);
         } catch (IOException e) {
             e.printStackTrace();
         }
-/*
-        tbLogMode = (ToggleButton)findViewById(R.id.tbLogMode);
-        tbLogMode.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-             @Override
-             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                 //Intent i =
 
-                 try {
-                     tdgMod.setLogging(isChecked);
-                 } catch (IOException e) {
-                     e.printStackTrace();
-                 }
+    }
 
-                 if(i!=null){
-                     startActivity(i);
-                 }
-
-             }
-         }
-
-        );*/
+    public void updateTextView(String toThis) {
+        TextView textView = (TextView)findViewById(R.id.textView);
+        textView.setText(toThis);
     }
 
     @RequiresApi(api = Build.VERSION_CODES.O)
